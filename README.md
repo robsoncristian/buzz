9ae9ed5aefe126aba0f8cff6cd7882991f7069e5
robsoncristian <eu@robsoncristian.com>
2017-07-02 23:48:23 -0300

    First!

---
+++ README.md
@@ -0,0 +1,22 @@
+### 
+
+
+
+[![Deploy to Rung](https://i.imgur.com/uijt57R.png)](https://app.rung.com.br/integration/Buzz/customize)
+
+![rung-cli 0.8.0](https://img.shields.io/badge/rung--cli-0.8.0-blue.svg?style=flat-square)
+![Buzz 1.0.0](https://img.shields.io/badge/Buzz-1.0.0-green.svg?style=flat-square)
+
+
+
+#### Parameters
+
+|Parameter | Type | Description |
+|----------|------|-------------|
+| `brand` | `Char(100)` | Qual é a marca que você deseja monitorar? |
+
+<img align="left" width="256" src="./icon.png" />
+
+##### Dependencies
+
+- `rung-cli`: `0.8.0`
---
+++ index.js
@@ -0,0 +1,78 @@
+import { create } from 'rung-sdk';
+import { Char, OneOf } from 'rung-sdk/dist/types';
+import Bluebird from 'bluebird';
+import agent from 'superagent';
+import promisifyAgent from 'superagent-promise';
+import { map, mergeAll } from 'ramda';
+
+const request = promisifyAgent(agent, Bluebird);
+
+const newsApiUrl = 'https://api.cognitive.microsoft.com/bing/v5.0/news/search';
+const newsApiToken = '<< token >>';
+
+const styles = {
+  container: {
+      fontFamily: 'Roboto, sans-serif',
+      fontSize: '12px',
+      marginTop: '-3px'
+  }
+};
+
+function getContent(newsTitle) {
+  return (
+      <div style={ styles.container }>
+        <p><b>Notícia</b></p>
+        <p>{newsTitle}</p>
+      </div>
+  );
+}
+
+function render(value) {
+
+  const newsTitle = value.name;
+  const newsUrl = value.url;
+
+  return { [newsUrl] : {
+    title: `Notícia - ${newsTitle}`,
+    content: getContent(newsTitle),
+    comment: 
+      `
+      ## Notícia
+      ### [${newsTitle}](${newsUrl})
+      `
+  }};
+}
+
+function main(context,done) {
+  const { brand } = context.params;
+
+  return request
+    .get(newsApiUrl)
+    .set('Ocp-Apim-Subscription-Key', newsApiToken)
+    .set('Accept', 'application/json')
+    .query({q:brand,mkt:'pt-BR',count:10,setLang:'pt',freshness:'Day',originalImg:'true'})
+    .then(({ body }) => {
+      const alerts = mergeAll(map(render, body.value));
+      done({ alerts });
+    })
+    .catch(() => {
+      done({ alerts: {} })
+    });
+}
+
+const params = {
+  brand: {
+      description: _('Qual é a marca que você deseja monitorar?'),
+      type: Char(100),
+      required: true
+  }
+};
+
+export default create(main, { 
+  params,
+  primaryKey: true,
+  sidebar: {
+    startDate: false,
+        endDate: false
+  } 
+});
